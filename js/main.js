e2c3589980d59f1f97c0642b3f9214df15e2e627
Jakub Kania <kubak.15@gmail.com>
2017-05-15 15:05:28 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -22,10 +22,89 @@ btn_up.addEventListener("click", function(e) {
 
 window.addEventListener("scroll", function() {
     
-    if (this.scrollY >= 500) {
+    if (this.scrollY >= 600) {
         btn_up.classList.remove("up-opa");
     } else {
         btn_up.classList.add("up-opa");
     }
     
 }, false);
+
+
+
+
+
+
+//menu
+
+var btnMenu = document.getElementById("nav-menu");
+btnMenu.addEventListener("click", toggleMenu);
+
+
+function toggleMenu(e) {
+    
+    var divOverlay = document.getElementsByClassName("overlay")[0];
+    var speed = 10;
+    
+    
+    var eventTarget = e.target;
+    
+    if( eventTarget.className == "btn-open" ) {
+        fadeIn(divOverlay, speed);
+        eventTarget.className = "btn-close";
+        
+    } else if ( eventTarget.className == "btn-close") {
+        fadeOut(divOverlay, speed);
+        eventTarget.className = "btn-open";
+    }
+    
+}
+
+function fadeIn(elem, speed) {
+    
+    var inInterval = setInterval(function(){
+        
+    elem.style.opacity = Number(elem.style.opacity) + 0.02;
+    
+    if (elem.style.opacity >= 1) {
+        
+        elem.style.opacity = 1;
+        clearInterval(inInterval);
+        
+    }    
+        
+        
+    }, speed); // 10ms == .01s
+    
+    
+    
+}
+
+function fadeOut(elem, speed) {
+    
+    var outInterval = setInterval(function(){
+        
+    elem.style.opacity = Number(elem.style.opacity) - 0.02;
+    
+    if (elem.style.opacity <= 0) {
+        
+        elem.style.opacity = 0;
+        clearInterval(outInterval);
+        
+    }    
+        
+        
+    }, speed); // 10ms == .01s
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
